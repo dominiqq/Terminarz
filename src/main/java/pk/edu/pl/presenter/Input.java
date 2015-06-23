3375c1b67076a96c40225a9b6e31af828a600beb
Łukasz <Thorin_10@interia.pl>
2015-06-23 23:10:38 +0200

    Dodano interface do pakietu pk.edu.pl.prezenter oraz klasę odpowiedzialną za interpretacje poleceń i infomacji z konsoli

---
+++ src/main/java/pk/edu/pl/presenter/Input.java
@@ -0,0 +1,5 @@
+package pk.edu.pl.presenter;
+
+public class Input implements presenter {
+	// klasa odpowiedzialna za interpretację poleceń i infomacji z konsoli
+}
---
+++ src/main/java/pk/edu/pl/presenter/presenter.java
@@ -0,0 +1,5 @@
+package pk.edu.pl.presenter;
+
+public interface presenter {
+
+}
