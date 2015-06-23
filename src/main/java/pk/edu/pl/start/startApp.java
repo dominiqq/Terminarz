0e4d0c937e3236b9f644a10e20436074e1142bd4
Ida <idalinnert@gmail.com>
2015-06-23 23:02:13 +0200

    Do projektu zostały dodane trzy pakiety. Wybraliśmy wzorzec  Model-View-Presenter (Model-Widok-Prezenter) Dodane pakiety: *pk.edu.pl.model - będzie zawierać klasę obudowaną w funkcje do obsługi pliku zawierającego dane o harmonogramie *pk.edu.pl.presenter - klasa zawierająca metody odppowiedzialne za wykonanie odpowiednich działań na podstawie poleceń od urzytkownika programu *pk.edu.pl.view - sposób wyświetlania informacji i komunikacja ze światem zewnętrznym

--- src/main/java/pk/edu/pl/start/startApp.java
+++ src/main/java/pk/edu/pl/start/startApp.java
@@ -3,7 +3,6 @@ package pk.edu.pl.start;
 public class startApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("== END ==");
 	}
 
---
+++ src/main/java/pk/edu/pl/view/view.java
@@ -0,0 +1,5 @@
+package pk.edu.pl.view;
+
+public interface view {
+	void launch();
+}
