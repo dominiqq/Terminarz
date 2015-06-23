5f780255d62df5ebe8a7351c49320743a9c15277
Ida <idalinnert@gmail.com>
2015-06-23 23:08:18 +0200

    Uzupełnione

---
+++ src/main/java/pk/edu/pl/view/terminal.java
@@ -0,0 +1,14 @@
+package pk.edu.pl.view;
+
+public class terminal implements view{
+
+	@Override
+	public void launch() {
+		// TODO Auto-generated method stub
+		System.out.println("== end ==");
+		
+	}
+
+
+	
+}
