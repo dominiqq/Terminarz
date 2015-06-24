a390dd06d39c221b211048044e31871bcf1451bc
Dominik <domini.q@wp.pl>
2015-06-24 13:32:15 +0200

    Scalone z szblonem, poprawiłem troszke menu.

--- src/main/java/pk/edu/pl/view/terminal.java
+++ src/main/java/pk/edu/pl/view/terminal.java
@@ -1,15 +1,25 @@
 package pk.edu.pl.view;
 
+import java.util.*;
+
 public class terminal implements view{
 
 	@Override
 	public void launch() {
 		int x =1;
+
+		Scanner keyboard = new Scanner(System.in); //tworzy obiekt czytajacy z klawiatury
+
+           
 		while(x == 1){
 			System.out.println("Wybierz opcje : ......");
 			// wczytanie z klawiatury
+			 x = keyboard.nextInt(); //zczytuje pierwszego inta
 			// uruchomienie odpowiedniej opcji
+			 if(x == 0)
+				 break;
 		}
+		keyboard.close();
 	}
 
 
